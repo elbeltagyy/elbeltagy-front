2d0ac8d3d18f7d6079c698267ca9af4f1d698453
elawadii <elawadym44@example.com>
2025-09-01 04:05:50 +0300

    reduce bundle size

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-// import { sentryVitePlugin } from "@sentry/vite-plugin";
-// , sentryVitePlugin({
-//   org: "elawadii",
-//   project: "elbeltagy"
-// })
+
 import react from '@vitejs/plugin-react';
 import { defineConfig, transformWithEsbuild } from 'vite';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+
+// import viteImagemin from 'vite-plugin-imagemin';
+// import { visualizer } from "rollup-plugin-visualizer"; // to Analysis compo
+// import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [{
     name: 'treat-js-files-as-jsx', // *js
     async transform(code, id) {
@@ -21,7 +22,25 @@ export default defineConfig({
       });
     },
   }, // pre
-  react()],
+  react(),
+  mode === 'production' &&
+  sentryVitePlugin({
+    org: 'elbeltagy',
+    project: 'elbeltagy',
+    authToken: import.meta.env.VITE_SENTRY_AUTH_TOKEN, // Use env for security
+    release: 'production', // Set dynamically
+    sourceMapReference: false, // Avoid source map bloat
+  }),
+    // Compress images
+    // viteImagemin({
+    //   gifsicle: { optimizationLevel: 3 },
+    //   optipng: { optimizationLevel: 7 },
+    //   mozjpeg: { quality: 75 },
+    //   pngquant: { quality: [0.65, 0.9], speed: 4 },
+    //   svgo: { plugins: [{ name: 'removeViewBox', active: false }] },
+    // }),
+  ].filter(Boolean), //, visualizer({ open: true }),, viteCompression({ algorithm: 'brotliCompress' })
+
   optimizeDeps: {
     // force: true,
     esbuildOptions: { // *for enableing js
@@ -38,6 +57,16 @@ export default defineConfig({
   },
 
   build: {
-    sourcemap: false
-  }
-});
+    sourcemap: mode === 'development',
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.webp'],
+}));
+
+//build:
+// minify: 'terser',
+//   terserOptions: {
+//     compress: {
+//       drop_console: true,
+//       drop_debugger: true,
+//     },
+//   },
